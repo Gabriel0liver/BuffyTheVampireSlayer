2d1e6f49ab91c0c4b7d2e30ce4102afbe6b026b5
CarloDubini <45142126+CarloDubini@users.noreply.github.com>
2020-10-29 22:12:54 +0100

    Update Slayer.java

--- src/org/ucm/tp1/p1/objetos/Slayer.java
+++ src/org/ucm/tp1/p1/objetos/Slayer.java
@@ -7,28 +7,52 @@ public class Slayer {
 	private int x,y;
 	private int life= 3;
 	private Game game;
+	private boolean alive;
 	public static final int COST= 50;
 	public static final int HARM = 1;
 	
 	
 	
-	public Slayer(int x, int y) {
+	public Slayer(Game game, int x, int y) {
 		this.x=x;
 		this.y=y;
+		this.game = game;
+		this.alive = true;
 		
 	}
-	public bool isAlive() {
+	public boolean isAlive( int i) {
 		
+		if (i <= 0) {
+			this.alive = false;
+		}
+		
+	return this.alive;	
 	}
+
+	
 	public void decreaseLife() {
+		this.life--;
 		
-		
+		isAlive(this.life);
 	}
 	public void attack() {
-		
+		int i = 0;
+		int shot = this.x;
+		boolean success = false;
+		while(shot < this.game.level.dimx  || success== false) { // longitud de x, esta en this.game.
+		while( i >this.game.board.vampireList.cont || success == false) {
+			if(this.game.board.vampireList.vampiros[i].y.equals(this.y) && this.game.board.vampireList.vampiros[i].x.equals(shot)) {
+				success= true;
+			}
+			
+			i++;
+		}
+		shot++;
+		}
 	}
 	public void toString{
 		
+				
 	}
 	
 	
@@ -42,4 +66,5 @@ public class Slayer {
 	
 	
 	
+}
 }
