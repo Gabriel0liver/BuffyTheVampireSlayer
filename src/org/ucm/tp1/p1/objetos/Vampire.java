fc6be8eeca4026c13767cd3bc0c74f286249029f
carlo dubini <carlo.dubini.mar@gmail.com>
2020-10-30 01:23:38 +0100

    estoy a medio camino de acabar los objetos :D

--- src/org/ucm/tp1/p1/objetos/Vampire.java
+++ src/org/ucm/tp1/p1/objetos/Vampire.java
@@ -7,31 +7,58 @@ public class Vampire {
 	private int y;
 	private int life= 5;
 	private Game game;
-	public static final int HARM=1;
+	public static final int HARM=1; //daño
 	public static final int SPEED = 2; // avanza 1 casilla cada 2 ciclos;
 	private Random rand;
+	boolean alive;
 	
-	public Vampire(int x, int y) {
+	public Vampire(Game game,int x, int y) {
 		this.x=x;
 		this.y=y;
+		this.game = game;
+		this.alive = true;
 		
 	}
-	public bool isAlive() {
+public boolean isAlive() {
 		
+		if (this.life <= 0) {
+			this.alive = false;
+		}
+		
+	return this.alive;	
 	}
-	public void decreaseLife() {
+	public void decreaseLife(int harm) {
+		this.life= this.life -harm;
 		
-		
+		isAlive();
 	}
+	
+	
 	public void move() {
+		// recuerda que el vampiro pasa por encima de los cazadores muertos
+		
+		
+		
 		
 	}
-	public void attack() {
+	public void attack(SlayerList slayerList, int i) {// el game tiene que ejecutar el ataque
+		
+		
+		
 		
 	}
 	public void toString{
 		
 	}
 	
+	
+	public int getX() {
+		
+		return this.x;
+	}
+public int getY() {
+		
+		return this.y;
+	}
 	
 }
